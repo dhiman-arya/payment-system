e455515eef2046ca575463171dd9122050e78735
Dhiman Arya <dhiman.arya@cimpress.com>
2026-09-12 14:55:28 +0530

    Added 3 web pages about, home and store

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.listen(port, () => console.log("server running on port " + port));
